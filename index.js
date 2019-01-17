b8cc849d3ba48212ff8ccb54390a55a9231f7d3e
Kiss, Aron <aron123dev@gmail.com>
2019-01-17 17:22:27 +0100

    Add core functions

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function Barion (options) {
-    this.options = options || {};
-
-    if (!this.options.POSKey) {
-        throw new Error('POSKey is required to communicate with Barion API.');
-    }
-
-    if (!this.options.environment) {
-        this.options.environment = 'test';
-    }
-}
-
-module.exports = Barion;
+module.exports = require('./lib/barion');
