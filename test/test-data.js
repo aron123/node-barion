95fca30455b0feacb7a7fb68f084175baaf62745
Kiss, Aron <aron123dev@gmail.com>
2019-01-20 13:49:00 +0100

    Add test data

---
+++ test/test-data.js
@@ -0,0 +1,28 @@
+/**
+ * These objects are used in tests.
+ */
+module.exports = {
+    fetchTest: {
+        successResponse: { 
+            Errors: [], 
+            success: true, 
+            stringVal: 'ok'
+        },
+        errorResponse: {
+            body: {
+                Errors: [
+                    { Title: 'Authentication failed.', HappenedAt: "2019-01-19T18:46:51.0808761Z" } 
+                ]
+            },
+            status: 400
+        },
+        internalErrorResponse: {
+            body: { Message: 'An error has occurred.' },
+            status: 500
+        },
+        notJsonResponse: '<html></html>',
+        networkErrorResponse: {
+            throws: 'Failed to fetch data'
+        }
+    }
+}
