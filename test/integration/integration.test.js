6212d52a33aeb72747c5642cc6aecb17626316b3
Kiss, Aron <aron123dev@gmail.com>
2019-01-24 16:28:49 +0100

    Add integration tests

---
+++ test/integration/integration.test.js
@@ -0,0 +1,206 @@
+let chai = require('chai');
+chai.use(require('chai-as-promised'));
+let expect = chai.expect;
+
+let Barion = require('../../'); //index.js in root level
+let testData = require('./test-data');
+
+describe('Integration tests', function () {
+    describe('Start payment', function () {
+
+        let barion;
+        let startPayment = testData.startPayment;
+        
+        beforeEach(function () {
+            barion = new Barion(testData.initOptions);
+        });
+
+        describe('should respond with response body on success', function () {
+            it('- Callback', function (done) {
+                barion.startPayment(startPayment.successRequestBody, (err, data) => {
+                    expect(data).to.deep.include(startPayment.successResponseBody);
+                    done();
+                });
+            });
+
+            it('- Promise', function () {
+                return expect(barion.startPayment(startPayment.successRequestBody))
+                    .to.eventually.deep.include(startPayment.successResponseBody);
+            });
+        });
+
+        describe('should respond with BarionError on wrong request body', function () {
+            it('- Callback', function (done) {
+                barion.startPayment(startPayment.errorRequestBody, (err) => {
+                    expect(err.name).to.equal('BarionError');
+                    expect(err.errors).to.have.lengthOf(1);
+                    expect(err.errors[0]).to.deep.include(startPayment.expectedError);
+                    done();
+                });
+            });
+
+            it('- Promise', function () {
+                return barion.startPayment(startPayment.errorRequestBody)
+                    .then(_ => { throw Error('Promise expected to reject, but resolved') })
+                    .catch(err => {
+                        expect(err.name).to.equal('BarionError');
+                        expect(err.errors).to.have.lengthOf(1);
+                        expect(err.errors[0]).to.deep.include(startPayment.expectedError);
+                    });
+            });
+        });
+    });
+
+    describe('Get payment state', function () {
+
+        let barion;
+        let getPaymentState = testData.getPaymentState;
+        let paymentId;
+
+        beforeEach(function (done) {
+            barion = new Barion(testData.initOptions);
+            barion.startPayment(testData.startPayment.successRequestBody, function (err, data) {
+                if (err) return done(err);
+                paymentId = data.PaymentId;
+                done();
+            });
+        });
+
+        afterEach(function () { 
+            paymentId = null;
+        });
+
+        describe('should respond with response body on success', function () {
+            it('- Callback', function (done) {
+                barion.getPaymentState({ paymentId }, function (err, data) {
+                    expect(data).to.deep.include(getPaymentState.successResponseBody);
+                    done();
+                });
+            });
+
+            it('- Promise', function () {
+                return expect(barion.getPaymentState({ paymentId }))
+                    .to.eventually.deep.include(getPaymentState.successResponseBody);
+            });
+        });
+
+        describe('should respond with BarionError on wrong request body', function () {
+            it('- Callback', function (done) {
+                barion.getPaymentState(getPaymentState.errorRequestBody, function (err, data) {
+                    expect(err.errors).to.have.lengthOf(2);
+                    expect(err.errors[0]).to.deep.include(getPaymentState.expectedErrors[0]);
+                    expect(err.errors[1]).to.deep.include(getPaymentState.expectedErrors[1]);
+                    done();
+                });
+            });
+
+            it('- Promise', function () {
+                return barion.getPaymentState(getPaymentState.errorRequestBody)
+                    .then(_ => { throw Error('Promise expected to reject, but resolved') })
+                    .catch(err => {
+                        expect(err.name).to.equal('BarionError');
+                        expect(err.errors).to.have.lengthOf(2);
+                        expect(err.errors[0]).to.deep.include(getPaymentState.expectedErrors[0]);
+                        expect(err.errors[1]).to.deep.include(getPaymentState.expectedErrors[1]);
+                    });
+            });
+        });
+    });
+
+    describe.skip('Finish reservation', function () {
+        // can't be tested automatically, I think,
+        // because typing bank card info on Barion GUI is required after the payment is started
+    });
+
+    describe('Bank transfer', function () {
+
+        let barion;
+        let bankTransfer = testData.bankTransfer;
+
+        beforeEach(function () {
+            barion = new Barion(testData.initOptions);
+        });
+
+        describe('should respond with response body on success', function () {
+            it('- Callback', function (done) {
+                barion.bankTransfer(bankTransfer.successRequestBody, (err, data) => {
+                    if (err) return done(err);
+                    expect(data).to.deep.include(bankTransfer.successResponseBody);
+                    done();
+                })
+            });
+
+            it('- Promise', function () {
+                return expect(barion.bankTransfer(bankTransfer.successRequestBody))
+                        .to.eventually.deep.include(bankTransfer.successResponseBody);
+            });
+        });
+
+        describe('should respond with BarionError on wrong request body', function () {
+            it('- Callback', function (done) {
+                barion.bankTransfer(bankTransfer.errorRequestBody, (err, data) => {
+                    expect(err.name).to.equal('BarionError');
+                    expect(err.errors).to.have.lengthOf(1);
+                    expect(err.errors[0]).to.deep.include(bankTransfer.expectedError);
+                    done();
+                })
+            });
+
+            it('- Promise', function () {
+                return barion.bankTransfer(bankTransfer.errorRequestBody)
+                    .then(_ => { throw Error('Promise expected to reject, but resolved') })
+                    .catch(err => {
+                        expect(err.name).to.equal('BarionError');
+                        expect(err.errors).to.have.lengthOf(1);
+                        expect(err.errors[0]).to.deep.include(bankTransfer.expectedError);
+                    });
+            });
+        });
+    });
+
+    describe('Barion transfer', function () {
+
+        let barion;
+        let barionTransfer = testData.barionTransfer;
+
+        beforeEach(function () {
+            barion = new Barion(testData.initOptions);
+        })
+
+        describe('should respond with response body on success', function () {
+            it('- Callback', function (done) {
+                barion.barionTransfer(barionTransfer.successRequestBody, (err, data) => {
+                    if (err) return done(err);
+                    expect(data).to.deep.include(barionTransfer.successResponseBody);
+                    done();
+                })
+            });
+
+            it('- Promise', function () {
+                return expect(barion.barionTransfer(barionTransfer.successRequestBody))
+                    .to.eventually.deep.include(barionTransfer.successResponseBody);
+            });
+        });
+
+        describe('should respond with BarionError on wrong request body', function () {
+            it('- Callback', function (done) {
+                barion.barionTransfer(barionTransfer.errorRequestBody, (err, data) => {
+                    expect(err.name).to.equal('BarionError');
+                    expect(err.errors).to.have.lengthOf(1);
+                    expect(err.errors[0]).to.deep.include(barionTransfer.expectedError);
+                    done();
+                });
+            });
+
+            it('- Promise', function () {
+                return barion.barionTransfer(barionTransfer.errorRequestBody)
+                    .then(_ => { throw Error('Promise expected to reject, but resolved') })
+                    .catch(err => {
+                        expect(err.name).to.equal('BarionError');
+                        expect(err.errors).to.have.lengthOf(1);
+                        expect(err.errors[0]).to.deep.include(barionTransfer.expectedError);
+                    });
+            });
+        });
+    });
+});
---
+++ test/integration/test-data.js
@@ -0,0 +1,162 @@
+/* 
+ * On CI system use environment variables,
+ * locally use a json file. 
+ */
+let POSKey = process.env.BARION_POS_KEY || require('./credentials.json').POSKey;
+let UserName = process.env.BARION_USER_NAME || require('./credentials.json').UserName;
+let Password = process.env.BARION_PASSWORD || require('./credentials').Password;
+
+module.exports = {
+    initOptions: {
+        POSKey: POSKey,
+        Environment: 'test'
+    },
+    startPayment: {
+        successRequestBody: {
+            "PaymentType": "Immediate",
+            "GuestCheckOut": true,
+            "FundingSources": [ "All" ],
+            "OrderNumber": "O-2019-0001",
+            "PaymentRequestId": "O-2019-0001-1",
+            "Transactions": [
+                {
+                    "POSTransactionId": "O-2019-0001",
+                    "Payee": UserName,
+                    "Total": 50,
+                    "Items": []
+                }
+            ],
+            "Locale": "hu-HU",
+            "Currency": "HUF"
+        },
+        successResponseBody: {
+            "PaymentRequestId": "O-2019-0001-1",
+            "Status": "Prepared",
+            "RecurrenceResult": "None",
+            "Errors": []
+        },
+        errorRequestBody: {
+            "PaymentType": "Immediate",
+            "GuestCheckOut": true,
+            "FundingSources": [ "All" ],
+            "OrderNumber": "O-2019-0001",
+            "PaymentRequestId": "O-2019-0001-1",
+            "Locale": "hu-HU",
+            "Currency": "HUF"
+        },
+        expectedError: {
+            "Title": "Model Validation Error",
+            "Description": "The Transactions field is required.",
+            "ErrorCode": "ModelValidationError",
+            "AuthData": UserName
+        }
+    },
+    getPaymentState: {
+        //successRequestBody: to be defined runtime, before run the test
+        successResponseBody: {
+            "PaymentRequestId": "O-2019-0001-1",
+            "OrderNumber": "O-2019-0001",
+            "POSOwnerEmail": UserName,
+            "Status": "Prepared",
+            "PaymentType": "Immediate",
+            "AllowedFundingSources": ["All"],
+            "GuestCheckout": true,
+            "Total": 50,
+            "SuggestedLocale": "hu-HU",
+            "Currency": "HUF",
+            "Errors": []
+        },
+        errorRequestBody: {},
+        expectedErrors: [
+            {
+                "Title": "Model Validation Error",
+                "Description": "The value 'undefined' is not valid for PaymentId.",
+                "ErrorCode": "ModelValidationError",
+                "AuthData": UserName,
+            },
+            {
+            "Title": "Model Validation Error",
+            "Description": "The PaymentId field is required.",
+            "ErrorCode": "ModelValidationError",
+            "AuthData": UserName
+            }
+        ]
+    },
+    bankTransfer: {
+        successRequestBody: {
+            "UserName": UserName,
+            "Password": Password,
+            "Currency": "HUF",
+            "Amount": 1,
+            "RecipientName": "xxxxx",
+            "BankAccount": {
+                "Country": "HUN",
+                "Format": "Giro",
+                "AccountNumber": "10032000-01076019"
+            }
+        },
+        successResponseBody: {
+            "Currency": "HUF",
+            "Amount": 1,
+            "RecipientName": "xxxxx",
+            "Comment": null,
+            "BankAccount": {
+                "Country": "HUN",
+                "Format": "Giro",
+                "AccountNumber": "10032000-01076019",
+                "Address": null,
+                "BankName": null,
+                "BankAddress": null,
+                "SwiftCode": null
+            },
+            "Errors": []
+        },
+        errorRequestBody: {
+            "UserName": UserName,
+            "Password": Password,
+            "Currency": "HUF",
+            "Amount": 1,
+            "RecipientName": "xxxxx",
+            "BankAccount": {
+                "Country": "HUN",
+                "Format": "Giro",
+                "AccountNumber": "10032000-01070000"
+            }
+        },
+        expectedError: {
+            "Title": "Model Validation Error",
+            "Description": "Incorrect bank account format",
+            "ErrorCode": "ModelValidationError",
+            "AuthData": UserName,
+        }
+    },
+    barionTransfer: {
+        successRequestBody: {
+            "UserName": UserName,
+            "Password": Password,
+            "Currency": "HUF",
+            "Amount": 1,
+            "Recipient": "info@example.com"
+        },
+        successResponseBody: {
+            "Amount": -1,
+            "ToName": "info@example.com",
+            "Currency": "HUF",
+            "TransactionType": 3,
+            "Direction": 0,
+            "ErrorList": []
+        },
+        errorRequestBody: {
+            "UserName": UserName,
+            "Password": Password,
+            "Currency": "HUF",
+            "Amount": 1,
+            "Recipient": "info#example.com"
+        },
+        expectedError: {
+            "ErrorNumber": 2004,
+            "ErrorMessage": "Invalid recipient format",
+            "AuthData": UserName
+        }
+    }
+};
