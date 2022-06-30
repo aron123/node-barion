0cd08cabc89cc680eabf9a6238807773557f8d24
Kiss, Aron <aron123dev@gmail.com>
2022-06-30 22:51:20 +0200

    Make options Partial

--- scripts/generate-typedef/node-barion.template.d.ts
+++ scripts/generate-typedef/node-barion.template.d.ts
@@ -10,36 +10,36 @@ export = Barion;
 declare class Barion {
     constructor(options: InitialOptions);
 
-    bankTransfer(options: BankTransferRequest, callback: (err: Error, data: any) => void): void;
-    bankTransfer(options: BankTransferRequest): Promise<object>;
+    bankTransfer(options: Partial<BankTransferRequest>, callback: (err: Error, data: any) => void): void;
+    bankTransfer(options: Partial<BankTransferRequest>): Promise<object>;
 
-    cancelAuthorizedPayment(options: CancelAuthorizationRequest, callback: (err: Error, data: any) => void): void;
-    cancelAuthorizedPayment(options: CancelAuthorizationRequest): Promise<object>;
+    cancelAuthorizedPayment(options: Partial<CancelAuthorizationRequest>, callback: (err: Error, data: any) => void): void;
+    cancelAuthorizedPayment(options: Partial<CancelAuthorizationRequest>): Promise<object>;
 
-    captureAuthorizedPayment(options: CapturePaymentRequest, callback: (err: Error, data: any) => void): void;
-    captureAuthorizedPayment(options: CapturePaymentRequest): Promise<object>;
+    captureAuthorizedPayment(options: Partial<CapturePaymentRequest>, callback: (err: Error, data: any) => void): void;
+    captureAuthorizedPayment(options: Partial<CapturePaymentRequest>): Promise<object>;
 
-    completePayment(options: CompletePaymentRequest, callback: (err: Error, data: any) => void): void;
-    completePayment(options: CompletePaymentRequest): Promise<object>;
+    completePayment(options: Partial<CompletePaymentRequest>, callback: (err: Error, data: any) => void): void;
+    completePayment(options: Partial<CompletePaymentRequest>): Promise<object>;
 
-    downloadStatement(options: StatementDownloadRequest, callback: (err: Error, data: any) => void): void;
-    downloadStatement(options: StatementDownloadRequest): Promise<object>;
+    downloadStatement(options: Partial<StatementDownloadRequest>, callback: (err: Error, data: any) => void): void;
+    downloadStatement(options: Partial<StatementDownloadRequest>): Promise<object>;
 
-    emailTransfer(options: EmailTransferRequest, callback: (err: Error, data: any) => void): void;
-    emailTransfer(options: EmailTransferRequest): Promise<object>;
+    emailTransfer(options: Partial<EmailTransferRequest>, callback: (err: Error, data: any) => void): void;
+    emailTransfer(options: Partial<EmailTransferRequest>): Promise<object>;
 
-    finishReservation(options: FinishReservationRequest, callback: (err: Error, data: any) => void): void;
-    finishReservation(options: FinishReservationRequest): Promise<object>;
+    finishReservation(options: Partial<FinishReservationRequest>, callback: (err: Error, data: any) => void): void;
+    finishReservation(options: Partial<FinishReservationRequest>): Promise<object>;
 
-    getAccounts(options: GetAccountsRequest, callback: (err: Error, data: any) => void): void;
-    getAccounts(options: GetAccountsRequest): Promise<object>;
+    getAccounts(options: Partial<GetAccountsRequest>, callback: (err: Error, data: any) => void): void;
+    getAccounts(options: Partial<GetAccountsRequest>): Promise<object>;
 
-    getPaymentState(options: GetPaymentStateRequest, callback: (err: Error, data: any) => void): void;
-    getPaymentState(options: GetPaymentStateRequest): Promise<object>;
+    getPaymentState(options: Partial<GetPaymentStateRequest>, callback: (err: Error, data: any) => void): void;
+    getPaymentState(options: Partial<GetPaymentStateRequest>): Promise<object>;
 
-    refundPayment(options: PaymentRefundRequest, callback: (err: Error, data: any) => void): void;
-    refundPayment(options: PaymentRefundRequest): Promise<object>;
+    refundPayment(options: Partial<PaymentRefundRequest>, callback: (err: Error, data: any) => void): void;
+    refundPayment(options: Partial<PaymentRefundRequest>): Promise<object>;
 
-    startPayment(options: StartPaymentRequest, callback: (err: Error, data: any) => void): void;
-    startPayment(options: StartPaymentRequest): Promise<object>;
+    startPayment(options: Partial<StartPaymentRequest>, callback: (err: Error, data: any) => void): void;
+    startPayment(options: Partial<StartPaymentRequest>): Promise<object>;
 }
